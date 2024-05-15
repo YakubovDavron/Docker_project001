fbad0073090bc69c2899b4dfb8496cf6730fc598
Davron <yakubovdavron090@gmail.com>
2024-05-15 18:00:12 +0500

    docker

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', HomeApiView.as_view(), name='home'),
+    path('posts/<int:pk>/', DetailApiView.as_view(), name='detail'),
+    path('posts/', PostApiView.as_view(), name='posts'),
+    path('about/', AboutApiView.as_view(), name='about'),
+    path('categories/', CategoryApiView.as_view(), name='categories'),
+    path('tags/', TagApiView.as_view(), name='tags'),
+    path('authors/', AuthorApiView.as_view(), name='authors'),
+    path('comments/', CommentApiView.as_view(), name='comments'),
+]
